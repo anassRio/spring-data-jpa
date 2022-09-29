f2fe5eae7a7d75f116c3d7a16b916afc030be449
Rouimi <rouimi.anass@gmail.com>
2022-09-29 18:58:47 +0200

    First Step
    Connect DB + Entity + Sequence + Columns + Constraints+ Repository + init first Data

---
+++ src/main/java/com/example/demo/domain/Student.java
@@ -0,0 +1,54 @@
+package com.example.demo.domain;
+
+import lombok.Data;
+import javax.persistence.*;
+
+import static javax.persistence.GenerationType.SEQUENCE;
+
+@Entity(name = "Student")
+@Table(
+        name = "student",
+        uniqueConstraints = {
+                @UniqueConstraint(name="student_email_unique", columnNames = "email")
+        }
+)
+@Data
+public class Student {
+    @Id
+    @SequenceGenerator(
+            name="student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            generator = "student_sequence",
+            strategy = SEQUENCE
+    )
+    @Column(name ="id",
+            updatable = false)
+    private Long id;
+    @Column(
+            name ="first_name",
+            nullable = false,
+            columnDefinition = "TEXT")
+    private String firstName;
+    @Column(
+            name ="last_name",
+            nullable = false)
+    private String lastName;
+    @Column(
+            name ="email",
+            nullable = false)
+    private String email;
+    @Column(
+            name ="age",
+            nullable = false)
+    private Integer age;
+
+    public Student(String firstName, String lastName, String email, Integer age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.age = age;
+    }
+}
